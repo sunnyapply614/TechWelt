a031c5de574555d3137a431783f794b00e0c4a69
katcherka <katcherka04@outlook.com>
2024-06-20 10:43:18 -0700

    file

---
+++ src/controllers/companies.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const { companySchema } = require("../models");
+
+module.exports = () => {
+  const addCompany = async (req, res) => {
+    const {
+        name,
+        username,
+        email,
+        mobile,
+        country_name,
+        address,
+    } = req.body;
+
+    if (!(name && username && email && mobile && country_name && address)) {
+      res.status(400).json({ message: "All Input is required" });
+      return;
+    }
+    try {      
+    //   const admin = await companySchema.findOne({ email });
+     const admin = 1;
+      if (!admin) {
+        res.status(400).json({ message: "Already same user exists!" });
+      } else {
+        await addCompany.save();
+      }
+      console.log("save success");
+    } catch (err) {
+      res.status(401).json({ message: "Something went wrong.", err: err });
+    }
+  };
+
+  return {
+    addCompany,
+  };
+};
