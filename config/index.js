4e42c5b351ed43af30b9a2e5a5e575666cda9bfb
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-05 04:41:14 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,3 @@
-const config = {
-  // mongoURI: "mongodb://Admin:Ubuntu111@127.0.0.1:27017/techwelt?authSource=admin",
-  mongoURI: "mongodb://localhost:27017/eyenia",
-};
+
 
 module.exports = config;
