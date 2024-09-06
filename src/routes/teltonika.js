182cd02538f1960d64d3bd0c6784de9687e91495
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-06 23:36:03 +0300

    Update teltonika.js

--- src/routes/teltonika.js
+++ src/routes/teltonika.js
@@ -4,7 +4,7 @@ module.exports = () => {
   const teltonika = require("../controllers/teltonika");
   const { createTeltonika } = teltonika();
 
-
+  router.post("/create", createTeltonika);
 
   return router;
 };
