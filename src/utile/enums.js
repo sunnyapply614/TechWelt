f8452d4b91c39b269d29458b37b151477b3fdc61
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-12 05:34:34 +0300

    Update enums.js

--- src/utile/enums.js
+++ src/utile/enums.js
@@ -8,7 +8,16 @@ module.exports = () => {
 
     const cmdType = {
 
-
+        EngineOn: "EngineOn",
+        DigitalOutput: "Digital Output",
+        DigitalInput: "Digital Input", 
+        BatteryLevel: "Battery Level",
+        BatteryVoltage: "Battery Voltage",
+        ExternalVoltage: "External Voltage",
+        AnalogueInput: "Analogue Input",
+        AxisX: "Axis X",
+        AxisY: "Axis Y",
+        AxisZ: "Axis Z",
         MotionDetection: "Motion Detection",
         SleepMode: "Sleep Mode",
         NetworkTimeProtocalServer: "Network Time Protocal Server",
