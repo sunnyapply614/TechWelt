d9506756b3195002e4a478676b67141d72695f8f
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-17 08:11:48 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,9 +16,7 @@ module.exports = () => {
   const randHex = function (len) {
     var maxlen = 32,
       min = Math.pow(16, Math.min(len, maxlen) - 1);
-    (max = Math.pow(16, Math.min(len, maxlen)) - 1),
-      (n = Math.floor(Math.random() * (max - min + 1)) + min),
-      (r = n.toString(16));
+
     while (r.length < len) {
       r = r + randHex(len - maxlen);
     }
