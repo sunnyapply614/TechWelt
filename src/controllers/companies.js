bf554707fe8ae982c92c85e64ba9d6d689b6caeb
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-22 12:09:16 +0300

    Update companies.js

--- src/controllers/companies.js
+++ src/controllers/companies.js
@@ -13,7 +13,8 @@ module.exports = () => {
     } = req.body;
         
     if (!(name && username && email && mobile && country_name && address)) {
-
+      res.status(400).json({ message: "All Input is required" });
+      return;
     }
 
     try {      
