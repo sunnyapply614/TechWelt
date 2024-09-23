29c0ad115e744bd94cc927b65b586d4532745ac8
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-23 13:08:56 -0700

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,7 +5,7 @@ const formData = require("form-data");
 const Mailgun = require("mailgun.js");
 var generator = require("generate-password");
 
-const { userSchema } = require("../models");
+
 
 const baseUrl =
   process.env.DEV == "Develop"
