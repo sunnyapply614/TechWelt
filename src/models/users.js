6da41d200162ea2198a6b81de1cad664b17b7328
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-27 04:39:20 +0300

    Update users.js

--- src/models/users.js
+++ src/models/users.js
@@ -16,4 +16,3 @@ const schema = new mongoose.Schema({
 	status: { type: Boolean, default: false },
 }, { timestamps: true })
 //role : admin, manager, client
-module.exports = mongoose.model("Users", schema)
