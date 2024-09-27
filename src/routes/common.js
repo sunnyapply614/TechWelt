fb91bfefb49f08e6374ef5b195f2017f853448af
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-27 04:42:00 +0300

    Update common.js

--- src/routes/common.js
+++ src/routes/common.js
@@ -6,9 +6,7 @@ module.exports = (router) => {
   const { uploadFile } = common();
 
   const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/avatars');
-    },
+
     filename: (req, file, cb) => {
       console.log("*************************");
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
