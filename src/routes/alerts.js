c6ce3512bc6d07e6ddc29a8922263e546121d73d
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-27 04:39:58 +0300

    Update alerts.js

--- src/routes/alerts.js
+++ src/routes/alerts.js
@@ -1,11 +1,1 @@
 
-module.exports = (router) => {
-  const alerts = require("../controllers/alerts")
-  const {
-    showAlertList,
-  } = alerts()
-
-  router.post("/list", showAlertList)    //get Alert list normal
-  
-  return router
-}
