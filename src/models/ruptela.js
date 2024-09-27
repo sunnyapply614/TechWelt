28492f985c9c436f8c5b6849f172d5edc5244cb6
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-27 04:37:16 +0300

    Update ruptela.js

--- src/models/ruptela.js
+++ src/models/ruptela.js
@@ -8,9 +8,7 @@ const IOvalue = new mongoose.Schema({
 
 module.exports = new mongoose.Schema(
 	{
-		deviceImei: { type: String, require: true },
-		deviceType: { type: String, default: "Ruptela" },
-		deviceModel: { type: String },
+
 		fw_version: { type: String, require: true },
 		transferDate: { type: Date, default: Date.now() },
 		lat: { type: Number, default: 0 },
