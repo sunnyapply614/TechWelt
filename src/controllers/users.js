4e6ec688bc68c7d933e8aa5409a94d7957c0d640
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-29 03:39:52 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,9 +1,4 @@
-require("dotenv").config();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const formData = require("form-data");
-const Mailgun = require("mailgun.js");
-var generator = require("generate-password");
+
 
 const { userSchema } = require("../models");
 
