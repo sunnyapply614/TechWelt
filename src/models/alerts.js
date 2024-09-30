370fc0b7d106f4ecdb81860bf2fba91bc6a403cf
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-30 18:57:53 +0300

    Update alerts.js

--- src/models/alerts.js
+++ src/models/alerts.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose")
 
-const schema = new mongoose.Schema({
-	userId:{type:String},
-	vehicle: {type:String, require:true},
-	alert: {type:String, require:true, unique:true},
-	time:{ type: Date, default: Date.now() }
-}, { timestamps: true })
+
 
 module.exports = mongoose.model("Alerts", schema)
