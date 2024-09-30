1d32d6382cfcff31339898eb192e318a9f452216
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-09-30 20:40:39 +0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,8 @@
-
+const userSchema = require('./users');
+const vehicleSchema = require('./vehicles');
+const teltonikaSchema = require('./teltonika');
+const ruptelaSchema = require('./ruptela');
+const commandSchema = require('./command');
 const alertSchema = require('./alerts')
 const geofenceSchema = require('./geofence')
 const reportSchema = require('./reports')
