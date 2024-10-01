734cdd5f6b0088b20f274b3febc8fbc8005aa54e
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-01 04:52:03 +0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,17 +5,7 @@ const router = express.Router();
 module.exports = () => {
   const auth = require("../controllers/users");
 
-  const {
-    signup,
-    login,
-    verifyEmail,
-    verifyChangeEmail,
-    reset,
-    getUserIdList,
-    resendEmailbyUserid,
-    resendEmailbyEmail,
-    validateUserName,
-  } = auth();
+
   router.post("/login", login);
   router.post("/signup", signup);
   router.post("/validateUserName", validateUserName);
