92a321812f2fd7fcaf75002f7f9ea1ece9dc4807
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-03 04:56:52 +0300

    Update companies.js

--- src/controllers/companies.js
+++ src/controllers/companies.js
@@ -19,7 +19,16 @@ module.exports = () => {
 
     try {      
     //   const admin = await companySchema.findOne({ email });        
-
+     const admin = 1;
+      if (!admin) {
+        res.status(400).json({ message: "Already same user exists!" });
+      } else {
+        await addCompany.save();
+      }
+      console.log("save success");
+    } catch (err) {
+      res.status(401).json({ message: "Something went wrong.", err: err });
+    }
   };
   return {
     addCompany,
