0f63f1342d4beb193db6c43cbda8bc906dd9fa2d
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-03 05:00:33 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -46,7 +46,15 @@ module.exports = () => {
 
   const addUser = async (req, res) => {
     const {
-
+      fname,
+      lname,
+      email,
+      phone,
+      country,
+      address,
+      role,
+      company,
+      image,
     } = req.body;
     const userId = req.user.id;
     if (!(email && fname && lname && userId)) {
