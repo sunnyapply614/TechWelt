68f7d992712308383b1a0b69f727a35fd6ce7dd9
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-03 04:54:18 +0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,7 @@
 const verifyToken = (req, res, next) => {
-
+  const jwt = require("jsonwebtoken")
+  const token = req.body.token || req.query.token || req.headers["x-access-token"]
+  const { TOKEN_SECRET } = process.env
   
   if (!token) {
     return res.status(403).send("A token is required for authentication")
