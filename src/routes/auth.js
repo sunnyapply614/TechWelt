1b854da111646baa975315c3ccbb884e46a39cc7
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-06 07:32:56 +0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,6 @@
-
+const express = require("express");
+const { verify } = require("jsonwebtoken");
+const router = express.Router();
 
 module.exports = () => {
   const auth = require("../controllers/users");
