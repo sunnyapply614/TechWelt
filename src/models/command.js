ab27d55d0bb7b7e6b6b30c0bc3db0baf3196c8c0
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-09 04:16:24 +0300

    Update command.js

--- src/models/command.js
+++ src/models/command.js
@@ -4,11 +4,7 @@ const { cmdStatus } = enums()
 
 const schema = new mongoose.Schema({
 
-	deviceImei: { type: String, require: true}, 
-	status: { type: String, default: cmdStatus.Queued }, 
-	cmdType: { type: String, require: true },
-	command: { type: String, require: true },
-	response: { type: String, default: "" }
+
 }, { timestamps: true })
 
 module.exports = mongoose.model("Command", schema)
