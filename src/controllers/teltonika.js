5c180157f1ad4df76355418688fd8001d9a967c3
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-10 11:51:19 +0300

    Update teltonika.js

--- src/controllers/teltonika.js
+++ src/controllers/teltonika.js
@@ -6,18 +6,7 @@ module.exports = () => {
     
     try {
       const { deviceImei, lat, lng, movement, speed, fuel, battery, signal, address, transferDate } = req.body
-      let data = new teltonikaSchema({
-        deviceImei: deviceImei,
-        lat: lat,
-        lng: lng,
-        transferDate: transferDate,
-        movement: movement,
-        speed: speed,
-        fuel: fuel,
-        battery: battery,
-        signal: signal,
-        address: address
-      });
+
       await data.save(function (err, savedDocument) {
         if (err) {
           console.error("save error:", err);
