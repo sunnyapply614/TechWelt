dd09921d79d57cc47a9d28ecd507ac318811af23
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-10 03:40:11 +0300

    Update alerts.js

--- src/controllers/alerts.js
+++ src/controllers/alerts.js
@@ -25,7 +25,9 @@ module.exports = () => {
       }
 
       // Apply filters
-
+      if (fromDate && toDate) {
+        alerts = alerts.filter(alert => alert.time >= fromDate && alert.time <= toDate);
+      }
       if (alertType) {
         alerts = alerts.filter(alert => alert.alert === alertType);
       }
