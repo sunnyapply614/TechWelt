6c479a3bf8b40a7dc7df64b782b9d949882eb07a
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-11 02:43:49 +0300

    Update companies.js

--- src/controllers/companies.js
+++ src/controllers/companies.js
@@ -1,16 +1,7 @@
 require("dotenv").config();
 const { companySchema } = require("../models");
 
-module.exports = () => {
-    const addCompany = async (req, res) => {
-    const {
-        name,
-        username,
-        email,
-        mobile,
-        country_name,
-        address,
-    } = req.body;
+
         
     if (!(name && username && email && mobile && country_name && address)) {
       res.status(400).json({ message: "All Input is required" });
