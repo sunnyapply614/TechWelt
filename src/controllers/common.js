26700ebb95efd80530a8372804f86eb28d8e5892
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-11 02:42:28 +0300

    Update common.js

--- src/controllers/common.js
+++ src/controllers/common.js
@@ -1,2 +1,13 @@
-
+const path = require('path');
+module.exports = () => {
+  const uploadFile = (req, res) => {
+      if (!req.file) {
+      return res.status(400).send('No file.');
+    }
+    res.send({ filePath });
+  }
+  
+  return { uploadFile }
+  
+}
 
