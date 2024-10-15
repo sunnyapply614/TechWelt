d9c070249f1377475acb3a8dc34ca4eb4417da75
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-15 15:39:00 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,16 +12,6 @@ module.exports = {
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'ec2-user',
-      host : 'ec2-103-4-8-90.ap-northeast-1.compute.amazonaws.com',
-      key  : '../social_experience_app_key.pem',
-      ref  : 'origin/develop',
-      repo : 'git@github.com:gentlyawesome/social_experience_app_dev.git',
-      path : '/home/ec2-user/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
+
   }
 };
