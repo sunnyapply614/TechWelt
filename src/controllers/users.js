67a171b02fb8386fed7983cdb1a80adab30db5c4
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-18 03:12:52 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -13,17 +13,7 @@ const baseUrl =
     : process.env.BASE_URL;
 
 module.exports = () => {
-  const randHex = function (len) {
-    var maxlen = 32,
-      min = Math.pow(16, Math.min(len, maxlen) - 1);
-    (max = Math.pow(16, Math.min(len, maxlen)) - 1),
-      (n = Math.floor(Math.random() * (max - min + 1)) + min),
-      (r = n.toString(16));
-    while (r.length < len) {
-      r = r + randHex(len - maxlen);
-    }
-    return r;
-  };
+
 
   const getUsers = async (req, res) => {
     try {
