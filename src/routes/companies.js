6e422906e17befc7d44a17c78dda8a56d8293b6d
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-19 16:17:06 +0300

    Update companies.js

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,10 +1,2 @@
-module.exports = (router) => {
-    const company = require("../controllers/companies")
-    const {
-       addCompany
-    } = company()
-    router.post("/addCompany", addCompany)
 
-    return router
-  }
   
