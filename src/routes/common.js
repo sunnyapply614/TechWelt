ff7d62d0852a27ce6a602ec93b7087c1276ac306
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-19 16:16:10 +0300

    Update common.js

--- src/routes/common.js
+++ src/routes/common.js
@@ -5,15 +5,7 @@ module.exports = (router) => {
   const common = require("../controllers/common");
   const { uploadFile } = common();
 
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/avatars');
-    },
-    filename: (req, file, cb) => {
-      console.log("*************************");
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
-  });
+
   
   const fileFilter = (req, file, cb) => {
     if (!file.mimetype.startsWith('image/')) {
