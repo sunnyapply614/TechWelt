6bee5589e7cc6dde63b6e1ed5c3c3ad01c689ef1
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-25 14:58:26 +0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,15 +4,7 @@ module.exports = (router) => {
     getUsers, addUser, updateUser, removeUser, activateUser, getUser, getGoogleAPIKey,changeEmail,updateAvatar
   } = users()
 
-  router.post("/getGoogleAPIKey", getGoogleAPIKey)
-  router.post("/getUser", getUser)
-  router.get("/list", getUsers)
-  router.post("/addUser", addUser)
-  router.post("/updateUser", updateUser)
-  router.post("/removeUser", removeUser)
-  router.post("/activateUser", activateUser)
-  router.post("/changeEmail", changeEmail)
-  require('./companies')(router);
+
 
   return router
 }
