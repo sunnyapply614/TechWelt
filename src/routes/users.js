048984a2ca4afb7b964898c79399b10e837c67d9
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-29 02:26:24 +0200

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 module.exports = (router) => {
-
+  const users = require("../controllers/users")
   const {
     getUsers, addUser, updateUser, removeUser, activateUser, getUser, getGoogleAPIKey,changeEmail,updateAvatar
   } = users()
