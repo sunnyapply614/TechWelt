95ed02ed753b4ed2f25c1e18b2bb7d9c717eaa09
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-29 02:23:57 +0200

    Update cronManager.js

--- src/services/cronManager.js
+++ src/services/cronManager.js
@@ -3,4 +3,11 @@ const vehicles = require('../controllers/vehicles');
 const { updateSatus } = vehicles();
 const jobMap = new Map();
 
+module.exports = () => {
+    const deviceStatusUpdateJob = cron.schedule('*/3 * * * *', async () => {
+        console.log("======================cron job========================")
+        await updateSatus();
+    }, { scheduled: false }).start();
 
+    jobMap.set('deviceStatusUpdateJob', deviceStatusUpdateJob);
+};
