7f41092b1385c65d77ef8ade8a6db5eeb052831a
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-29 02:28:15 +0200

    Update companies.js

--- src/models/companies.js
+++ src/models/companies.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose")
 
-const schema = new mongoose.Schema({
-    name: {type: String, require: true},
-    username: {type: String, require: true, unique: true},
-    email: {type: String, require: true},
-    mobile: {type: String, defalut: ""},
-    country_name: {type: String, defalut: ""},
-    address: {type: String, defalut: ""},
-}, { timestamps: true })
+
 
 module.exports = mongoose.model("Companies", schema)
