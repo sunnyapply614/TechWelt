5b20939929bb65cef8296070a63f9c2415cdc6f2
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-10-30 03:54:50 +0200

    Update teltonika.js

--- src/controllers/teltonika.js
+++ src/controllers/teltonika.js
@@ -18,11 +18,7 @@ module.exports = () => {
     try {
       const { deviceImei, lat, lng, movement, speed, fuel, battery, signal, address, transferDate } = req.body
       await data.save(function (err, savedDocument) {
-        if (err) {
-          console.error("save error:", err);
-        }else{
-          res.status(200).json({ message:'teltonika data added' })
-        }
+
       });      
     } catch (err) {
       console.log(err)
