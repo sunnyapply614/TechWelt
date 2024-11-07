41524d057b59d9908b77d3532859d94e706d11f1
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-07 16:46:07 +0200

    Update teltonika.js

--- src/controllers/teltonika.js
+++ src/controllers/teltonika.js
@@ -25,7 +25,8 @@ module.exports = () => {
         }
       });      
     } catch (err) {
-
+      console.log(err)
+      res.status(401).json({ message: "Something went wrong", err })
     }
   }
   return { createTeltonika }
