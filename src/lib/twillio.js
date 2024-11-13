7b7e5f811eee55c6a30b96104a0c36d6c5cfaa09
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-13 03:08:30 +0200

    Update twillio.js

--- src/lib/twillio.js
+++ src/lib/twillio.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 const twilio = require('twilio');
 const client = new twilio(process.env.TWILIOSID, process.env.TWILIOTOKEN)
 
-
+// const message = async (body, to) => {
+//     try{
+//         const sid = await client.messages.create({body, to, from : process.env.TWILIONUMBER });
+//         return sid;
+//     }catch(err){
+//         throw err;
+//     }
+// }
 
 const message = async (to, from, body) => {
     try {
