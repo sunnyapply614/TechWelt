e342ea8aa613d17f09d307a52bc165f78b558e53
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-13 03:08:42 +0200

    Update smtp.js

--- src/lib/smtp.js
+++ src/lib/smtp.js
@@ -1,11 +1,5 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'support@vehtechs.com',
-    pass: 'Test@1234'
-  }
-});
+
 
 module.exports = { transporter };
